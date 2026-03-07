d32d7ab0da961a0a301b37668f8a7ab59d0a000e
Joe <joe@zerocapital.com>
2026-03-06 18:09:09 -0700

    chore: update __init__.py exports and lint fixes for tier 1

--- data/fnm/scripts/__init__.py
+++ data/fnm/scripts/__init__.py
@@ -1,1 +1,27 @@
 from __future__ import annotations
+
+from fnm.scripts.fnm_gating import (
+    FnmFileCheck,
+    FnmFileStatus,
+    FnmPathResult,
+    FnmPathStatus,
+    find_repo_root,
+    load_fnm_manifest,
+    resolve_fnm_path,
+)
+from fnm.scripts.fnm_gating_cli import cli_validate_fnm_path
+from fnm.scripts.fnm_gating_fixtures import require_fnm, require_fnm_csvs, require_fnm_raw
+
+__all__ = [
+    "FnmFileCheck",
+    "FnmFileStatus",
+    "FnmPathResult",
+    "FnmPathStatus",
+    "cli_validate_fnm_path",
+    "find_repo_root",
+    "load_fnm_manifest",
+    "require_fnm",
+    "require_fnm_csvs",
+    "require_fnm_raw",
+    "resolve_fnm_path",
+]
